bd6befa4695d86e12cf716b5ea23dfd38ec42209
Bret Walker <bw@bretw.com>
2021-05-22 09:39:28 -0400

    Move to provided GITHUB_TOKEN

--- scrape_all.py
+++ scrape_all.py
@@ -33,7 +33,7 @@ def discover_scrapers(token):
 
 
 if __name__ == "__main__":
-    github_token = os.getenv("GH_TOKEN")
+    github_token = os.getenv("GITHUB_TOKEN")
     for scraper in discover_scrapers(github_token):
         if github_token is None:
             scraper.test_mode = True
